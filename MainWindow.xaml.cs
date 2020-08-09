be006d6e069bcc95e05e06bd01257ae654a2f1b6
HyunwooHa <g830812@gmail.com>
2020-08-09 20:53:53 +0900

    Initial project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ExtremeEnviroment.Module.ImageList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExtremeEnviroment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitLayout();
+        }
+
+        private void InitLayout()
+        {
+            ImageListControl imageListControl = new ImageListControl();
+            
+            DockPanel.SetDock(imageListControl, Dock.Left);
+            mainPanel.Children.Add(imageListControl);
+        }
+    }
+}
---
+++ Module/ImageList/ImageListControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExtremeEnviroment.Module.ImageList
+{
+    /// <summary>
+    /// ImageListControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ImageListControl : UserControl
+    {
+        public ImageListControl()
+        {
+            InitializeComponent();
+            InitControl();
+        }
+        void InitControl()
+        {
+            this.AddItem(this.GetTestItem());
+        }
+
+        private TreeViewItem GetTestItem()
+        {
+            TreeViewItem treeViewItem = new TreeViewItem();
+            treeViewItem.Header = "Root";
+
+            TreeViewItem treeViewItem1 = new TreeViewItem();
+            treeViewItem1.Header = "CHILD1";
+            TreeViewItem treeViewItem2 = new TreeViewItem();
+            treeViewItem2.Header = "CHILD2";
+            TreeViewItem treeViewItem3 = new TreeViewItem();
+            treeViewItem3.Header = "CHILD3";
+
+            treeViewItem.Items.Add(treeViewItem1);
+            treeViewItem.Items.Add(treeViewItem2);
+            treeViewItem.Items.Add(treeViewItem3);
+
+            return treeViewItem;
+        }
+
+        public void AddItem(TreeViewItem treeViewItem)
+        {
+            ImageTree.Items.Add(treeViewItem);
+        }
+    }
+}
