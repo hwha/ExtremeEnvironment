f645d127b6f16556db535516707d7e1a0953f94d
KarlJeong <behappy0723@gmail.com>
2020-10-28 01:57:22 +0900

    수정 : 이미지속성 변경
    이미지 속성 Class 사용
    이미지 속성 수정시 속성창과 이미지창에서 모두 변경
    이미지 속성 삭제시 속성창과 이미지창에서 모두 변경
    이미지 속성 추가시 송성창과 이미지창에서 모두 변경
    이미지 속성 추가시 중복 Key확인

--- Module/ImagePropView/ImagePropViewControl.xaml.cs
+++ Module/ImagePropView/ImagePropViewControl.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
+using System.Security.Cryptography;
+using ExtremeEnviroment.Model;
+using System.Collections;
+using System.Linq;
+using System.Collections.ObjectModel;
 using ExtremeEnviroment.Module.ImageList;
 
 
@@ -21,9 +26,19 @@ namespace ExtremeEnviroment.Module.ImagePropView
     /// </summary>
     public partial class ImagePropViewControl : UserControl
     {
+        public ObservableCollection<PropsItem> currentPropsItem;
+
+        public ObservableCollection<PropsItem> CurrentPropsItem
+        {
+            get { return this.currentPropsItem; }
+        }
+
+        public PropsItem SelectedItem;
+
         public ImagePropViewControl()
         {
             InitializeComponent();
+            currentPropsItem = new ObservableCollection<PropsItem>();
         }
 
         private void DgImageProp_Loaded(object sender, RoutedEventArgs e)
@@ -33,42 +48,146 @@ namespace ExtremeEnviroment.Module.ImagePropView
 
         public void SetImageProps(Dictionary<String, String> metadataMap)
         {
-            DataTable dataTable = new DataTable();
 
-            dataTable.Columns.Add("PROP", typeof(string));
-            dataTable.Columns.Add("VALUE", typeof(string));
-
-            foreach (KeyValuePair<String, String> entry in metadataMap) 
+            for (int index = 0; index < metadataMap.Count; index++)
             {
-                dataTable.Rows.Add(new object[] { entry.Key, entry.Value });
+                var item = metadataMap.ElementAt(index);
+                this.AddRow(index, new PropsItem
+                {
+                    PROP = item.Key,
+                    VALUE = item.Value
+                });
             }
-
-
-
-            DgImageProp.ItemsSource = dataTable.DefaultView;
-            
         }
+
+        public void AddRow(int index, Dictionary<string, string> row)
+        {
+            this.AddRow(index, new PropsItem(row));
+        }
+
+
+        public void AddRow(int index, PropsItem item)
+        {
+            item.INDEX = index;
+            item.NUM = DgImageProp.Items.Count + 1;
+            currentPropsItem.Add(item);
+            this.RefreshDataGrid();
+        }
+
+        public void UpdateRow(int index, PropsItem item)
+        {
+            if (!currentPropsItem.Any(d => d.PROP == item.PROP))
+            {
+                this.AddRow(index, item);
+            }
+            else
+            {
+                currentPropsItem[index] = item;
+            }
+            this.RefreshDataGrid();
+
+        }
+
+
+        private void RefreshDataGrid()
+        {
+            this.DgImageProp.ItemsSource = null;
+            this.DgImageProp.ItemsSource = this.currentPropsItem;
+        }
+
+
 
         private void DgImageProp_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
         {
-            MainWindow mainWindow = ExtremeEnviroment.MainWindow._mainWindow;
+            PropsItem editedPropsItem = null;
 
-            TextBlock props = DgImageProp.Columns[0].GetCellContent(DgImageProp.Items[e.Row.GetIndex()]) as TextBlock;
-            TextBox value = e.EditingElement as TextBox;
+            int edtiedCellIndex = e.Column.DisplayIndex;
 
-            if (props != null && value != null)
+            if (edtiedCellIndex == 0)
             {
-                ImageListControl imageListControl = mainWindow.GetImageListControl();
+                TextBox prop = e.EditingElement as TextBox;
+                TextBlock value = DgImageProp.Columns[1].GetCellContent(DgImageProp.Items[e.Row.GetIndex()]) as TextBlock;
 
-                imageListControl.UpdateTreeItem(props.Text, value.Text);
+                if (currentPropsItem.Any(d => d.PROP == prop.Text)) 
+                {
+                    MessageBox.Show("동일한 속성이 존재합니다.", "Notice", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.RefreshDataGrid();
+                    return;
+                }
+
+                if (prop != null && value != null)
+                {
+                    editedPropsItem = new PropsItem
+                    {
+                        PROP = prop.Text,
+                        VALUE = value.Text
+                    };
+                }
+                    
             }
+            else if (edtiedCellIndex == 1)
+            {
+                TextBlock prop = DgImageProp.Columns[0].GetCellContent(DgImageProp.Items[e.Row.GetIndex()]) as TextBlock;
+                TextBox value = e.EditingElement as TextBox;
+                if (prop != null && value != null)
+                {
+                    editedPropsItem = new PropsItem
+                    {
+                        PROP = prop.Text,
+                        VALUE = value.Text
+                    };
+                }
+            }
+
+            if (!String.IsNullOrEmpty(editedPropsItem.PROP) && !String.IsNullOrEmpty(editedPropsItem.VALUE))
+            {
+                
+                this.UpdateRow(e.Row.GetIndex(), editedPropsItem);
+                this.updateTreeItem();
+            }
+
         }
 
-        private void DgImageProp_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        private void btbClick_Delete(object sender, RoutedEventArgs e)
         {
-            if (e.Row.Item != null) 
+            var selectedItems = DgImageProp.SelectedItems.Cast<PropsItem>().ToList();
+
+            foreach (PropsItem item in selectedItems)
             {
+                currentPropsItem.Remove(item);
             }
+            this.RefreshDataGrid();
+            this.updateTreeItem();
         }
+
+        private void updateTreeItem() 
+        {
+            MainWindow mainWindow = ExtremeEnviroment.MainWindow._mainWindow;
+            ImageListControl imageListControl = mainWindow.GetImageListControl();
+            imageListControl.SetTreeItem(currentPropsItem.Where(d => !String.IsNullOrEmpty(d.PROP))
+                .Select(d => new PropsItem { PROP = d.PROP, VALUE = d.VALUE, INDEX = d.INDEX, NUM = d.NUM })
+                .ToDictionary(v => v.PROP, v => v.VALUE));
+        }
+    }
+    public class PropsItem
+    {
+        public PropsItem()
+        {
+        }
+        public PropsItem(Dictionary<string, string> keyValuePairs)
+        {
+            this.FromDictionary(keyValuePairs);
+        }
+        public int INDEX { get; set; }
+        public int NUM { get; set; }
+        public string PROP { get; set; }
+        public string VALUE { get; set; }
+
+        public void FromDictionary(Dictionary<string, string> keyValuePairs)
+        {
+            this.PROP = keyValuePairs.GetValueOrDefault("PROP").ToString();
+            this.VALUE = keyValuePairs.GetValueOrDefault("VALUE").ToString();
+        }
+
     }
 }
